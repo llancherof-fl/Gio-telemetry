65b945c96cd3b2ae486885db8e4ba9ca95222b31
llancherof-fl <llanchero.f@gmail.com>
2026-03-17 15:50:04 -0500

    descripcion del cambio

--- server_aws_final.py
+++ server_aws_final.py
@@ -221,7 +221,7 @@ def index():
     </head>
     <body>
         <div class="header">
-            <h1>&#x1F4E1; GIO Telemetry Dashboard Darwin</h1>
+            <h1>&#x1F4E1; GIO Telemetry Dashboard PRUEBA LAURA VS AHORA</h1>
             <div class="meta">Servidor: <strong>{server_name}</strong> &mdash; PostgreSQL RDS &mdash; AWS</div>
         </div>
 
