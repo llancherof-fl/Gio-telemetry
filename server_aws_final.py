d73ba0c9300498ce43222c660e78f7b0b91e66e7
llancherof-fl <llanchero.f@gmail.com>
2026-03-17 19:00:05 -0500

    cambio

--- server_aws_final.py
+++ server_aws_final.py
@@ -221,7 +221,7 @@ def index():
     </head>
     <body>
         <div class="header">
-            <h1>&#x1F4E1; GIO Telemetry Dashboard SI FUNCIONO DISENO</h1>
+            <h1>&#x1F4E1; GIO Telemetry Dashboard</h1>
             <div class="meta">Servidor: <strong>{server_name}</strong> &mdash; PostgreSQL RDS &mdash; AWS</div>
         </div>
 
