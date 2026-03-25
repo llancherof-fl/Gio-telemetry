9781bb0a24980db9593e2f0165e9eeb92eeadcd4
lauraguerrero75 <lauraguerrero7752@gmail.com>
2026-03-24 23:19:39 -0500

    titulo cambiado

--- server_aws_final.py
+++ server_aws_final.py
@@ -193,7 +193,7 @@ def index():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{EC2_NAME} · GIO Telemetry</title>
+    <title>{EC2_NAME} · GIO GRUPO 2 Telemetry</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
